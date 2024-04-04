67d1ee28669932f60c5b27a916efd34ac20ac61e
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2024-04-04 13:09:33 +0300

    initial

---
+++ src/routes.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Error from "./pages/Error/Error";
+import Movie from "./pages/Movie/Movie";
+import TvSeries from "./pages/TvSeries/TvSeries";
+import Bookmark from "./pages/Bookmark/Bookmark";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/movies",
+    element: <Movie />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/tv-series",
+    element: <TvSeries />,
+    errorElement: <Error />,
+  },
+  {
+    path: "bookmarks",
+    element: <Bookmark />,
+    errorElement: <Error />,
+  },
+]);
